62f5a8150dfb9fb886204667ea7e428fa9a4f5ac
Manaj <80015153+Mann1102@users.noreply.github.com>
2023-09-08 22:10:34 +0530

    Add files via upload

---
+++ loginsystem/partials/_dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+    $username="root";
+    $password="";
+    $servername="localhost";
+    $dbname="users";
+
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
